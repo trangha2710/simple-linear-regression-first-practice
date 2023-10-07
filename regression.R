8ea8c3a6c56c322426b34f336661acdd0df31ff3
trangha2710 <101734306+trangha2710@users.noreply.github.com>
2023-10-07 14:52:07 +0700

    Add files via upload

--- regression.R
+++ regression.R
@@ -1,13 +1,11 @@
 #tai cac packages
 install.packages("ggplot2")
 install.packages("dplyr")
-install.packages("broom")
 
 #tai cac libraries
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-library(broom)
 library(haven)
 
 #doc file regression
@@ -17,26 +15,30 @@ View(df)
 #phan tich tuong quan
 cov(df$c38, df$wage, method ="pearson")
 cor(df$c38, df$wage, method ="pearson")
-plot(df$c38, df$wage, xlab = "c38", ylab = "wage")
-abline(lm(wage~c38, data= df), col = "Red")
+
 
 #linear regression 1 sample
 reg1 <- lm(wage ~ c38, data = df)
-reg1
-model1 <- lm(df$wage ~ df$c38)
-summary(model1)
+summary(reg1)
+plot(df$c38, df$wage, xlab = "c38", ylab = "wage", col = "blue")
+abline(reg1, col = "Red")
 
-#linear reg for multiple 
-dt = sort(sample(nrow(regression),nrow(regression)*.5))
-df1 <- regression[dt,]
-df2 <- regression[-dt,]
+#phan tich hoi quy cho tung mau
+dt = sort(sample(nrow(df),nrow(df)*.5))
+df1 <- df[dt,]
+df2 <- df[-dt,]
 
 dim(df1)
 dim(df2)
 
-model2 <- lm(df1$wage ~ df1$c38)
-model3 <- lm(df2$wage ~ df2$c38)
-plot(df1$c38, df1$wage, xlab = "c38", ylab = "wage")
-abline(lm(wage~c38, data= df1), col = "Blue")
-plot(df2$c38, df2$wage, xlab = "c38", ylab = "wage")
-abline(lm(wage~c38, data= df2), col = "Green")
+reg2 <- lm(df1$wage ~ df1$c38)
+reg3 <- lm(df2$wage ~ df2$c38)
+
+#danh gia cac tham so cho tung mo hinh hoi quy mau
+summary(reg2)
+summary(reg3)
+
+plot(df$c38, df$wage, xlab = "c38", ylab = "wage", col = "blue")
+abline(reg1, col = "Red")
+abline(reg2, col = "Yellow")
+abline(reg3, col = "Green")
